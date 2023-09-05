204f3be835348e88ff92e253beac3d2c85a95904
Leopold Lemmermann <leopold@lemmermann.com>
2023-09-05 18:21:37 +0200

    Passing stores by environment instead of env object now.

--- Current/test/Current-tests.swift
+++ Current/test/Current-tests.swift
@@ -7,15 +7,4 @@ import XCTest
 @MainActor
 final class CurrentTests: XCTestCase {
   // TODO: Add unit tests for Current
-
-  func test<#Feature#>_given<#Precondition#>_when<#Action#>_then<#Outcome#>() async throws {
-    <#Given#>
-
-    let store = TestStore(initialState: <#State#>, reducer: Current()) {
-      <#Dependencies#>
-    }
-
-    await store.send(<#Action#>) { <#New State#> }
-    await store.receive(<#Action#>) { <#New State#> }
-  }
 }
